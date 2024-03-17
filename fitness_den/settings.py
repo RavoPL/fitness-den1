36c2f0d56892254167e5d51abbf40d3e0f74a69c
RavoPL <ravosilesia88@protonmail.com>
2024-03-17 19:41:56 +0000

    environ e-mail switch test

--- fitness_den/settings.py
+++ fitness_den/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'False'
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = os.getenv('DEVELOPMENT')
 
 ALLOWED_HOSTS = ['localhost', 'fitness-den-01445db6e33a.herokuapp.com', '8000-ravopl-fitnessden1-cvmtg8bi8yq.ws-eu110.gitpod.io']
 
@@ -238,9 +238,9 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-if 'DEVELOPMENT' in os.environ:
+if os.getenv('DEVELOPMENT'):
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'fitnessden@example.com'
+    DEFAULT_FROM_EMAIL = 'r3m4k3w@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
